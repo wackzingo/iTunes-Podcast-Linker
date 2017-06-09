3b26b52d2b0c67c366cb4dd9eb1a08e560233d9d
wackzingo <wingoz@uw.edu>
2017-06-09 01:03:25 -0700

    Create iTunes-Podcast-Linker.js

--- iTunes-Podcast-Linker.js
+++ iTunes-Podcast-Linker.js
@@ -1,15 +1,30 @@
+// ==UserScript==
+// @name         iTunes-Podcast-Linker
+// @namespace    http://athleo.net/
+// @version      1.0
+// @description  It replaces the links to listen to a podcast in iTunes with a link directly to the video or mp3 when viewing an iTunes podcast page on the web.
+// @author       Zach Wingo
+// @match        http://itunes.apple.com
+// @grant        none
+// ==/UserScript==
 /**
  * This simple bookmarklet inserts a direct link
  * to listen to a particular podcast next to each
  * podcast on an iTunes page.
  */
-javascript: (function () { 
-	// This gets a list of all the rows in the podcast table.
-	var tableRows = document.getElementsByClassName("podcast-episode");
+(function () { 
+        'use strict';
 
-	// Here we loop through the list of rows, grab the url to the podcast, and insert a link.
-	for(var i = 0; i < tableRows.length; i++) {
-		var url = tableRows[i].getAttribute("audio-preview-url");
-		tableRows[i].insertCell(0).innerHTML = "<a href=\""+ url +"\">Listen</a>";
-	}
-    }());
+	var tableRows = document.getElementsByClassName("podcast-episode"); 
+
+	for(var i = 0; i < tableRows.length; i++) { 
+
+		var lastCell = tableRows[i].cells.length - 1; 
+		tableRows[i].deleteCell(lastCell);
+
+		var url = tableRows[i].getAttribute("audio-preview-url"); 
+
+		url = (url === null) ? url = tableRows[i].getAttribute("video-preview-url"): url;
+
+		tableRows[i].insertCell(lastCell).innerHTML = "<a href=\""+ url +"\">Listen</a>"; } 
+	 })();
