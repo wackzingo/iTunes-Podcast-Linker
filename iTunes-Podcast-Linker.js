e18615e37bef19386c47afdb2ed16efaa381c850
wackzingo <wingoz@uw.edu>
2017-06-09 01:22:24 -0700

    Create iTunes-Podcast-Linker.js

--- iTunes-Podcast-Linker.js
+++ iTunes-Podcast-Linker.js
@@ -1,17 +1,13 @@
 // ==UserScript==
 // @name         iTunes-Podcast-Linker
-// @namespace    http://athleo.net/
+// @namespace    http://athleo.net
 // @version      1.0
 // @description  It replaces the links to listen to a podcast in iTunes with a link directly to the video or mp3 when viewing an iTunes podcast page on the web.
 // @author       Zach Wingo
-// @match        http://itunes.apple.com
+// @match        https://itunes.apple.com/*
 // @grant        none
 // ==/UserScript==
-/**
- * This simple bookmarklet inserts a direct link
- * to listen to a particular podcast next to each
- * podcast on an iTunes page.
- */
+
 (function () { 
         'use strict';
 
@@ -27,4 +23,4 @@
 		url = (url === null) ? url = tableRows[i].getAttribute("video-preview-url"): url;
 
 		tableRows[i].insertCell(lastCell).innerHTML = "<a href=\""+ url +"\">Listen</a>"; } 
-	 })();
+})();
